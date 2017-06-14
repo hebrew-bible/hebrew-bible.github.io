746037a961afa259c057be551e0351c1470f05e8
Jose Luis Quiroga <joseluisquiroga@yahoo.com>
2017-06-14 08:40:34 -0500

    first commit

---
+++ one_html/generate_pdf.sh
@@ -0,0 +1,3 @@
+
+../wkhtmltox/bin/wkhtmltopdf --page-size Letter --margin-left 3 --margin-right 3 --margin-top 5 --margin-bottom 10 --footer-spacing 0 --footer-center [page]/[topage] ./FULL_HEBREW_BIBLE.html ./HEBREW_BIBLE.pdf
+
